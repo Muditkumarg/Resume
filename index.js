41411a1160ea240b7fc48cbe0f38eef302034082
Mudit Kumar <mudit@victoriousinfotech.com>
2024-07-11 11:53:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,134 @@
+// let name = "Rajat";
+
+// console.log(name);
+
+// var age = 25;
+
+// console.log(age);
+
+// const address = "Noida";
+
+// console.log(address);
+
+// let x;
+// console.log(typeof x);
+
+// let y = null;
+// console.log(typeof y);
+
+// Array:- array is a collection of item or data;
+
+let data = [10, 20, 30, 40, 50, 60];
+
+// console.log(data[3]);
+
+// push():- It is use to add new element in the last of array;
+
+// data.push(70);
+
+// console.log(data);
+
+// pop():- It is use remove the last element from an array;
+
+// data.pop();
+// console.log(data);
+
+// unshift():- This method is use to add a new element in the starting of an array.
+
+// data.unshift(0);
+// console.log(data);
+
+// data.shift();
+
+// data.shift();
+
+// console.log(data);
+
+// Object:- object is a collection of key and value pair.
+
+let person = {
+  name: "Mudit",
+  age: 25,
+  address: "Noida",
+};
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.address);
+
+// let { name, age, address } = person;
+// console.log(name);
+// console.log(age);
+// console.log(address);
+
+// Function:-function is code of block. that are use to write a saprate code.
+
+// function Person() {
+//   console.log("I am Mudit Kumar");
+// }
+
+// Person();
+
+// parameter:- parameter are use to access the value in the function.
+
+// function Person1(name, age) {
+//   console.log("My name is", name);
+//   console.log("My age is ", age);
+// }
+// Person1("Akash", 25);
+
+// Spread Operator:-spread operator is use to expand an array,
+
+// let data1 = ["Mudit", "Akash", "Rohit"];
+
+// let newData = [...data1, "Mohit"];
+
+// console.log(newData);
+
+// if-else:- it is a conditionally .it have two block like as if and else. when condition is true then execute
+// if block and condition is false then execute else block.
+
+// let day = 3;
+
+// let dayName;
+
+// switch (day) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     break;
+//   case 3:
+//     dayName = "Thursday";
+//     break;
+//   case 4:
+//     dayName = "Friday";
+//     break;
+// }
+// console.log(dayName);
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// for (let i = 0; i <= 3; i++) {
+//   if (i % 2) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let num = 5;
+
+// while (num <= 15) {
+//   console.log(num);
+//   num++;
+// }
+
+// let num = 10;
+
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 15);
